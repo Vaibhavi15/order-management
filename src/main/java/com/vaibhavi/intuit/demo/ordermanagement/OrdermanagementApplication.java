b6a46b518080ddad9d7d6d5e14201f55ffb47ed4
Vaibhavi Pai <paivaibhavi15@gmail.com>
2021-05-08 15:14:13 +0530

    feature: add order placing functionality

--- src/main/java/com/vaibhavi/intuit/demo/ordermanagement/OrdermanagementApplication.java
+++ src/main/java/com/vaibhavi/intuit/demo/ordermanagement/OrdermanagementApplication.java
@@ -2,12 +2,20 @@ package com.vaibhavi.intuit.demo.ordermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class OrdermanagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrdermanagementApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate getRestTemplate()
+	{
+		return new RestTemplate();
 	}
 
 }
--- src/main/java/com/vaibhavi/intuit/demo/ordermanagement/service/OrderPlaceServiceImpl.java
+++ src/main/java/com/vaibhavi/intuit/demo/ordermanagement/service/OrderPlaceServiceImpl.java
@@ -1,14 +1,40 @@
 package com.vaibhavi.intuit.demo.ordermanagement.service;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.vaibhavi.intuit.demo.ordermanagement.entity.Order;
 
 @Service
 public class OrderPlaceServiceImpl implements OrderPlaceService {
 
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Value("${order-place-service.url}")
+	String ORDER_PLACE_SERVICE_URL;
+	
 	@Override
 	public Order placeOrder(Order productOrder) {
-		return productOrder;
+		
+		/*Post dummyPost = new Post();
+		dummyPost.setUserId(15);
+		dummyPost.setBody("This is my 1000th post muaahahaha");*/
+		
+		HttpHeaders headers = new HttpHeaders();
+	    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+	    
+	    HttpEntity<Order> entity = new HttpEntity<Order>(productOrder,headers);
+	      
+	    return restTemplate.exchange(ORDER_PLACE_SERVICE_URL + "/posts", HttpMethod.POST, entity, Order.class).getBody();
+	 
 	}
 }
